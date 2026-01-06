7cd8f457450dc11d473b6ea079d1482e3313f80e
KARMAKER SONDIPON <25-61502-1@student.aiub.edu>
2026-01-06 19:26:48 +0600

    updated

--- Entity/Property.java
+++ Entity/Property.java
@@ -1,60 +1,73 @@
 package Entity;
 
-public class Property {
+public class Property{
     private String propertyID;
-    private String location;
-    private String type;            
-    private double price;
-    private double size;
-    private String status;
-
-    public Property(){
-
-    }
-
-    public Property(String propertyID, String location, String type, double price, double size, String status) {
-        this.propertyID = propertyID;
-        this.location = location;
-        this.type = type;
-        this.price = price;
-        this.size = size;
-        this.status = status;
-    }
-
-    public String getPropertyID() {
+	private String location;
+	private String type;
+	private double price;
+	private double size;
+	private String status;
+	
+	public Property(){
+		
+	}
+	
+	public Property(String propertyID,String location,String type,double price,double size,String status)
+	{
+	this.propertyID = propertyID;
+	this.location = location;
+	this.type = type;
+	this.price = price;
+	this.size = size;
+	this.status = status;
+	}
+public String getPropertyID() 
+	{
         return propertyID;
     }
-    public void setPropertyID(String propertyID) {
+    public void setPropertyID(String propertyID)
+	{
         this.propertyID = propertyID;
     }
-    public String getLocation() {
+    public String getLocation() 
+	{
         return location;
     }
-    public void setLocation(String location) {
+    public void setLocation(String location)
+	{
         this.location = location;
     }
-    public String getType() {
+    public String getType()
+	{
         return type;
     }
-    public void setType(String type) {
+    public void setType(String type)
+	{
         this.type = type;
     }
-    public double getPrice() {
+    public double getPrice() 
+	{
         return price;
     }
-    public void setPrice(double price) {
+    public void setPrice(double price) 
+	{
         this.price = price;
     }
-    public double getSize() {
+    public double getSize() 
+	{
         return size;
     }
-    public void setSize(double size) {
+    public void setSize(double size)
+	{
         this.size = size;
     }
-    public String getStatus() {
+    public String getStatus() 
+	{
         return status;
     }
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    public void setStatus(String status)
+	{
+		this.status = status;
+	}
 }
+
